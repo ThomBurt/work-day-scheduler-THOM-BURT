c6a655000d6ccf1203e016bb3218247478c7184a
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2021-12-30 14:51:12 -0500

    index2 and colors working

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,4 +1,54 @@
-// var currentDate = document.querySelector('#currentDay')
-
 // Todays date
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+
+// Color changes on time change
+var currentTime = moment().format("h");
+console.log(currentTime);
+
+
+//Change textarea background color based on time       https://stackoverflow.com/questions/62462599/how-can-i-change-elements-style-based-on-the-time-of-day
+var checkTime = function () {
+
+    //Get Current time
+    var currentTime = moment().format('H');
+
+    //get all elements with class "taskarea"
+    var timeBlockElements = $(".textarea");
+
+    //loop through taskarea classes
+    for (var i = 0 ; i < timeBlockElements.length ; i++) {
+
+        //Get element i's ID as a string
+        var elementID = timeBlockElements[i].id;
+
+        //get element by ID
+        var manipID = document.getElementById(timeBlockElements[i].id)
+
+        //remove any old classes from element
+        $(timeBlockElements[i].id).removeClass(".present .past .future");
+
+        // apply new class if task is present/past/future
+        if (elementID < currentTime) {
+            $(manipID).addClass("past");
+        } else if (elementID > currentTime) {
+            $(manipID).addClass("future");
+        } else {
+            $(manipID).addClass("present");
+        }
+    }
+}
+
+// checkTime every 5 minutes
+setInterval(checkTime(), (1000 * 60) * 5);
+
+
+
+
+
+
+
+// saving into local storage  TODO
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
